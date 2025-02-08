5cafa1e6b6cd8883a65f7a9b2274c7d09947e183
William Mays <>
2025-02-08 19:05:31 +0000

    Homepage updates

--- components/navbar/NavbarAdmin.tsx
+++ components/navbar/NavbarAdmin.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 /* eslint-disable */
 import Image from 'next/image';
 import AdminNavbarLinks from './NavbarLinksAdmin';
@@ -14,43 +13,25 @@ export default function AdminNavbar(props: {
 
   return (
     <nav
-      className={`fixed top-0 left-0 z-[10] flex w-full flex-row items-center justify-between bg-white/30 py-2 px-4 backdrop-blur-xl transition-all dark:bg-transparent`}
+      className={`fixed top-0 left-0 z-[10] flex w-full flex-row items-center justify-between bg-white/30 py-4 px-8 md:px-32 backdrop-blur-xl transition-all dark:bg-transparent`}
     >
       <div className="ml-[6px]">
-        <div className="h-10 md:mb-0 md:w-[224px] md:pt-0">
-          {/* <a
-            className="hidden text-xs font-normal text-zinc-950 hover:underline dark:text-white dark:hover:text-white md:inline"
-            href=""
-          >
-            Pages
-            <span className="mx-1 text-xs text-zinc-950 hover:text-zinc-950 dark:text-white">
-              {' '}
-              /{' '}
-            </span>
-          </a> */}
+        <div className="h-14 md:mb-0 md:w-[284px] md:pt-0">
           <NavLink
             className="text-xs font-normal capitalize text-zinc-950 hover:underline dark:text-white dark:hover:text-white"
             href="/"
           >
-            {/* {brandText} */}
             <Image
-              width="108"
-              height="40"
-              className="w-[108px] h-[40px]"
+              width="162"
+              height="60"
+              className="w-[122px] h-[60px]"
               src={TinselLinkLogo.src}
               alt="TinselLink"
               priority
             />
+
           </NavLink>
         </div>
-        {/* <p className="text-md shrink capitalize text-zinc-950 dark:text-white md:text-3xl">
-          <NavLink
-            href="#"
-            className="font-bold capitalize hover:text-zinc-950 dark:hover:text-white"
-          >
-            {brandText}
-          </NavLink>
-        </p> */}
       </div>
       <div className="w-[154px] min-w-max md:ml-auto md:w-[unset]">
         <AdminNavbarLinks />
